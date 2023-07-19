8f395d8bfde4e0f808eba4524a60864a572240e1
Aditya <adityapassi01@gmail.com>
2023-07-20 00:32:03 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Register from "./pages/Register";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import { Route, Routes } from "react-router-dom";
